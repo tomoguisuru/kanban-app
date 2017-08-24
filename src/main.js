26bf6d8e2490f6c5dedae66d0410c251350b6f2e
Brandon Drake <brandon.drake@teem.com>
2017-08-24 13:31:35 -0600

    Migrated from standard

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import config from '../config/environment';
 
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
@@ -9,6 +9,6 @@ const App = Ember.Application.extend({
   Resolver
 });
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix + '/src/init');
 
 export default App;
